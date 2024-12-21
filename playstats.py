881c662083c316a5f15f43a98bf38910214aadfa
Allen Huo <ahthing99@gmail.com>
2024-12-20 23:15:31 -0500

    added fpts calculator in playstats

--- playstats.py
+++ playstats.py
@@ -2,8 +2,10 @@ from basketball_reference_web_scraper import client
 from datetime import datetime
 
 class PlayerStats():
+    values = {}
 
     def __init__(self):
+        self.values = {"points":0.5,"assists":1,"rebounds":1,"turnovers":-1,"steals":2,"blocks":2,"3PM":0.5}
         return
     
     #Generate player ids
@@ -11,6 +13,17 @@ class PlayerStats():
         for i in client.players_season_totals(season_end_year=year):
             if i['name'] == name:
                 return i['slug']
+    
+    #Calculate a players total FPTS given a box score
+    def calculate_fantasy_points(self,box_score):
+        return box_score['points_scored'] * self.values['points'] + \
+        (box_score['offensive_rebounds'] + box_score['defensive_rebounds']) * self.values['rebounds'] + \
+        box_score['assists'] * self.values['assists'] + \
+        box_score['turnovers'] * self.values['turnovers'] + \
+        box_score['steals'] * self.values['steals'] + \
+        box_score['blocks'] * self.values['blocks'] + \
+        box_score['made_three_point_field_goals'] * self.values['3PM']
+
 
     
     #Generate Box Scores for given player for a season - returns list of box scores (dictionaries)
