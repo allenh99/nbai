792d375998fbdcb310f0e53001e54bc8e2bcbafc
allenh99 <ahthing99@gmail.com>
2024-12-28 21:30:30 -0500

    boxScoreAgent return added

--- agents/boxscore_agent.py
+++ agents/boxscore_agent.py
@@ -8,7 +8,7 @@ formatted_date = current_date.strftime("%m/%d/%Y")
 def boxScoreAgent(data):
     player = data["player"]
     ou = data["o/u"]
-    number = data["number"]
+    number = float(data["number"])
     stat_type = data["stat_type"]
 
     generator = PlayerIDGenerator(2025)
@@ -16,5 +16,27 @@ def boxScoreAgent(data):
 
     stats = PlayerStats()
     averages = stats.calculate_stat_averages(player,formatted_date)
+    periods = ["1week", "1month", "3month"]
 
-    score = 0
+    performance_comparison = {}
+
+    for period in periods:
+        period_average = averages[period][stat_type]["average"]
+        season_average = averages["season"][stat_type]["average"]
+
+        if period_average > season_average:
+            performance_comparison[period] = "better"
+        elif period_average < season_average:
+            performance_comparison[period] = "worse"
+        else:
+            performance_comparison[period] = "same"
+
+    recent_form_average = averages["1week"][stat_type]["average"]
+    if ou == "o":
+        score = recent_form_average >= number
+    else:
+        score = recent_form_average <= number
+
+    ret = 'over' if score else 'under'
+
+    return {data: performance_comparison, message: ["Based on the last week, {ret} {number} for {stat_type} is expected."], signal: ret}
--- scrape.py
+++ scrape.py
@@ -1,3 +1,3 @@
 from agents.boxscore_agent import boxScoreAgent
 
-boxScoreAgent({"player":"Josh Giddey","o/u":"o","number":"8.5","stat_type":"rebounds","opp":"Washington Wizards"})
+boxScoreAgent({"player":"Josh Hart","o/u":"o","number":"8.5","stat_type":"rebounds","opp":"Washington Wizards"})
