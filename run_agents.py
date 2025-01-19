ed028e0ba2be59582c18667752f49134bd54d1f1
allenh99 <ahthing99@gmail.com>
2025-01-19 13:30:58 -0500

    updated run_agents to simulate manager

--- run_agents.py
+++ run_agents.py
@@ -1,11 +1,15 @@
 import sys
 from agents.manager_agent import managerAgent
+from agents.boxscore_agent import boxScoreAgent
+from agents.opp_agent import oppAgent
 
-player = sys.argv[1]
-ou = "o" if sys.argv[2].lower() == "over" else "u"
-number = sys.argv[3]
-stat_type = sys.argv[4]
-opp = sys.argv[5]
+
+
+player = sys.argv[1] + " " + sys.argv[2]
+ou = "o" if sys.argv[3].lower() == "over" else "u"
+number = float(sys.argv[4])
+stat_type = sys.argv[5]
+opp = sys.argv[6] + " " + sys.argv[7]
 
 #{'player': 'Josh Hart', 'ou': 'o', 'number': '8.5', 'stat_type': 'rebounds', 'opp': 'Toronto Raptors'}
 
@@ -17,4 +21,9 @@ run_data = {
     'opp':opp
 }
 
-managerAgent()
+agents = {
+    "box_score_agent": boxScoreAgent,
+    "opp_agent": oppAgent
+}
+
+res = managerAgent(run_data,agents)
